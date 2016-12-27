c1e4949b31af02a8623dcfc98fa906efbb27a87f
Arne Skaanes <arne@amskonsult.no>
2016-12-27 16:00:54 +0100

    Initial commit avter publishing

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arne
+ * Date: 24/02/16
+ * Time: 20:23 ***********
+ */
+?>
+<footer>
+<div class="footer">
+    <ul id="nav">
+        <li><a href="/">hjem/home </a></li>
+        <li><a href="/periscopevision/">periscope vision</a></li>
+        <li><a href="/kildespeilet">the periscope/kildespeilet</a></li>
+        <li><a href="/observasjoner">observasjoner/observations</a></li>
+    </ul>
+    <p style="margin-left: 2.0em">Copyright 2016 Bodvar Schjelderup &copy;2016 &nbsp;&nbsp;www.periscopevision.org / www.bodvarschjelderup.org</p>
+</div>
+
---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<div class="header">
+    <div class="topp">www.bodvarschjelderup.org</div>
+    <ul id="nav">
+        <li><a href="/">hjem/home </a></li>
+        <li><a href="/periscopevision/">periscope vision</a></li>
+        <li><a href="/kildespeilet">the periscope/kildespeilet</a></li>
+        <li><a href="/observasjoner">observasjoner/observations</a></li>
+    </ul>
+</div>
