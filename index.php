92b4cd2a979b8d90df66648f5b7012ce9c5f14e9
Arne Skaanes <arne@amskonsult.no>
2016-12-27 16:03:25 +0100

    Initial commit avter publishing

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+    <meta name="description" content="Bodvar Schjelderup’s discoveries and interpretations"/>
+    <meta name="author" content="Bodvar Schjelderup"/>
+    <meta name="keywords" content="kildespeilet, periscope, pyramid, mercator"/>
+    <link href="includes/styles.css" rel="stylesheet" type="text/css"/>
+    <link href="//cloud.webtype.com/css/69607097-073d-4768-8705-cf18e5bba52e.css" rel="stylesheet" type="text/css"/>
+    <title>bodvar schjelderup / periscope vision</title>
+</head>
+<body>
+<div class="wrapper">
+    <?php include('./includes/header.php'); ?>
+    <div class="section">
+        <h1>bodvar schjelderup<br>kildespeilet / periscope vision</h1>
+        <div class="subText">Velg ett av de tre temaene nedenfor / choose one of the three themes below</div>
+        <div class="flexBox">
+
+            <div class="frontBox">
+                <a href="./periscopevision">
+                    <div class="linkBox">
+                        <h4>periscope vision</h4>
+                        Temabokser<br>
+                        Themes<br>
+                        <img src="./images/periscopevision.jpg" class="menuIll" alt=""/>
+                    </div>
+                </a>
+            </div>
+
+            <div class="frontBox">
+                <a href="./kildespeilet">
+                    <div class="linkBox">
+                        <h4>kildespeilet<br/>the periscope</h4>
+                        Utstillingsplansjer<br>
+                        Exhibition charts
+                        <img src="./images/kildespeilet.jpg" class="menuIll" alt=""/>
+                    </div>
+                </a>
+            </div>
+
+            <div class="frontBox">
+                <a href="./observasjoner">
+                    <div class="linkBox">
+                        <h4>observasjoner<br/>observations</h4>
+                        Hefter etc.; artikler; bokutdrag<br>
+                        Pamphlets; articles; excerpts
+                        <img src="./images/observasjoner.jpg" class="menuIll" alt=""/>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+    <?php include('./includes/footer.php'); ?>
+</div>
+</body>
+</html>
